fe92d9222f01946aa82ce9a62dc5000f006924ab
Chat <chatkamol.ketd@bumail.net>
2024-04-25 05:49:59 +0700

    First Commit and Add Movement Player

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    private float moveInput;
+    public float spd;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = 2;
+        }
+
+        //for jump
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(moveInput * spd, rb2d.velocity.y); //Code For Move
+    }
+
+}

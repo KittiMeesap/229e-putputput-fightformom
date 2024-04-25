4ce88e18bd304b08bd5316339f8795727c6e225e
kitti <tkt.ms@hotmail.com>
2024-04-25 08:05:59 +0700

    Update Charactet

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,55 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float spd = 1f;
+
     Rigidbody2D rb2d;
-    private float moveInput;
-    public float spd;
-    public float jumpForce;
-
-    private bool isGrounded;
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask whatIsGround;
-
-    private int extraJumps;
-    public int extraJumpsValue;
-
+    float horizontalVaule;
+    [SerializeField] bool isRunning = false;
+    bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        extraJumps = extraJumpsValue;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isGrounded == true)
-        {
-            extraJumps = 2;
-        }
+        horizontalVaule = Input.GetAxisRaw("Horizontal");
 
-        //for jump
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
-        {
-            rb2d.velocity = Vector2.up * jumpForce;
-            extraJumps--;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
-        {
-            rb2d.velocity = Vector2.up * jumpForce;
-        }
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+            isRunning = true;
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            isRunning = false;
+        
     }
-
-
     private void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
-        moveInput = Input.GetAxisRaw("Horizontal");
-        rb2d.velocity = new Vector2(moveInput * spd, rb2d.velocity.y); //Code For Move
+        Move(horizontalVaule);
     }
 
+    //Code for Move
+    void Move(float dir)
+    {
+        //Set value X using dir and speed
+        float xVal = dir * spd * 100 *  Time.deltaTime;
+        //Create Vec2 for the velocity
+        Vector2 targetVelocity = new Vector2(dir,rb2d.velocity.y);
+        //Player Velocity
+        rb2d.velocity = targetVelocity;
 
+        //flip to left
+        if (facingRight && dir < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            facingRight = false;
+        }
+        //flip to right
+        else if (!facingRight && dir > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            facingRight = true;
+        }
+
+    }
 }

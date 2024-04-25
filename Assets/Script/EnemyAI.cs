03069f82c8a701cbea18286273121033423fc8c3
kitti <tkt.ms@hotmail.com>
2024-04-25 11:39:37 +0700

    Update EnemyAI Walk

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> points;
-    public int nextID;
+    public int nextID=0;
     int idChangeValue = 1;
+    public float speed = 2f;
 
     private void Reset()
     {
@@ -28,12 +29,43 @@ public class EnemyAI : MonoBehaviour
         GameObject waypoints = new GameObject("Waypoints");
 
         waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position = root.transform.position;
 
-        GameObject p1 = new GameObject("Point 1"); p1.transform.SetParent(waypoints.transform);
-        GameObject p2 = new GameObject("Point 2"); p2.transform.SetParent(waypoints.transform);
+        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform); p1.transform.position = root.transform.position;
+        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform); p2.transform.position = root.transform.position;
 
         points = new List<Transform>();
         points.Add(p1.transform);
         points.Add(p2.transform);
+    }
+
+    private void Update()
+    {
+        MoveToNextPoint();
+    }
+
+    void MoveToNextPoint()
+    {
+        Transform goalPoint = points[nextID];
+
+        if (goalPoint.transform.position.x > transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+
+        transform.position = Vector2.MoveTowards(transform.position,goalPoint.position,speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position,goalPoint.position) < 1f)
+        {
+            if (nextID == points.Count -1)
+            {
+                idChangeValue = -1;
+            }
+
+            if (nextID == 0)
+                idChangeValue = 1;
+
+            nextID += idChangeValue;
+        }
     }
 }

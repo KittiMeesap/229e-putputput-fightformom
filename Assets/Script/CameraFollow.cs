f9b1e1d4437211f87d2dd7c7770795d2cd8fb3f6
kitti <tkt.ms@hotmail.com>
2024-04-25 09:24:40 +0700

    Update Camera follow 0.2

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -6,8 +6,9 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-    [Range(2,10)]
+    [Range(1,10)]
     public float smoothFactor;
+    public Vector3 minValuse, maxValue;
 
     private void FixedUpdate()
     {
@@ -17,6 +18,9 @@ public class CameraFollow : MonoBehaviour
     void Follow()
     {
         Vector3 targetPosition = target.position + offset;
+
+        Vector3 boundPosition = new Vector3(Mathf.Clamp(targetPosition.x,minValuse.x, maxValue.x), Mathf.Clamp(targetPosition.y, minValuse.y, maxValue.y), Mathf.Clamp(targetPosition.z, minValuse.z, maxValue.z));
+
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
         transform.position = targetPosition;
     }

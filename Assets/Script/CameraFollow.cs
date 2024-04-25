076574922bcb9852e85830504b8366eba3db31be
kitti <tkt.ms@hotmail.com>
2024-04-25 16:35:02 +0700

    Fix Throw System 0.6

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -19,20 +19,14 @@ public class CameraFollow : MonoBehaviour
     {
         Vector3 targetPosition = target.position + offset;
 
-        Vector3 boundPosition = new Vector3(Mathf.Clamp(targetPosition.x,minValuse.x, maxValue.x), Mathf.Clamp(targetPosition.y, minValuse.y, maxValue.y), Mathf.Clamp(targetPosition.z, minValuse.z, maxValue.z));
-
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
-        transform.position = targetPosition;
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-            return;
+        Vector3 clampedPosition = new Vector3(
+            Mathf.Clamp(smoothedPosition.x, minValuse.x, maxValue.x),
+            Mathf.Clamp(smoothedPosition.y, minValuse.y, maxValue.y),
+            Mathf.Clamp(smoothedPosition.z, minValuse.z, maxValue.z)
+        );
 
-        if (collision.GetComponent<ShootingAction>())
-            collision.GetComponent<ShootingAction>().Action();
-
-        Destroy(gameObject);
+        transform.position = clampedPosition;
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -152,4 +152,5 @@ public class PlayerController : MonoBehaviour
 
         return velocity;
     }
+    
 }

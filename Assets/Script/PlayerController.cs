cc2e6cfbfa008704ed63ec1a176af316ac59f325
kitti <tkt.ms@hotmail.com>
2024-04-25 14:07:23 +0700

    Update Shoot System 0.4

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+
 
     Rigidbody2D rb2d;
     Animator animator;
@@ -54,112 +54,122 @@ public class PlayerController : MonoBehaviour
         //Jump Input
         if (Input.GetButtonDown("Jump"))
             jump = true;
-        else if(Input.GetButtonUp("Jump"))
+        else if (Input.GetButtonUp("Jump"))
             jump = false;
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.magenta, 8f);
-            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
-
-            if (hit.collider != null)
+            // Assuming you have variables such as 'target', 'shootPoint', and 'bulletPrefabs' defined elsewhere
+            if (target != null && shootPoint != null && bulletPrefabs != null)
             {
-                target.transform.position = new Vector2(hit.point.x, hit.point.y);
-                Debug.Log($"hit point : {hit.point.x} , {hit.point.y}");
-
-                Vector2 projecttile = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+                Vector2 targetPosition = target.transform.position;
+                Vector2 projectile = CalculateProjectileVelocity(shootPoint.position, targetPosition, 1f);
                 Rigidbody2D fireBullet = Instantiate(bulletPrefabs, shootPoint.position, Quaternion.identity);
-                fireBullet.velocity = projecttile;
+                fireBullet.velocity = projectile;
             }
+
         }
 
-
     }
 
-    bool CanMove()
-    {
-        bool can = true;
+        bool CanMove()
+        {
+            bool can = true;
 
-        if (isDead)
-            can = false;
+            if (isDead)
+                can = false;
 
-        return can;
-    }
-    private void FixedUpdate()
-    {
-        GroundCheck();
-        Move(horizontalVaule,jump);
-    }
+            return can;
+        }
+        private void FixedUpdate()
+        {
+            GroundCheck();
+            Move(horizontalVaule, jump);
+        }
 
-    void GroundCheck()
-    {
-        isGrounded = false;
-
-        Collider2D[] collider = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
-        if(collider.Length > 0)
-            isGrounded = true;
-    }
-
-    //Code for Move
-    void Move(float dir,bool jumpFlag)
-    {
-        //Jump
-
-        if(isGrounded && jumpFlag)
+        void GroundCheck()
         {
             isGrounded = false;
-            jumpFlag = false;
-            rb2d.AddForce(new Vector2(0f, jumpPower));
+
+            Collider2D[] collider = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
+            if (collider.Length > 0)
+                isGrounded = true;
         }
 
-        //Move and Run
-        //Set value of x using dir and speed
-        float xVal = dir * spd * 100 * Time.fixedDeltaTime;
-
-        if (isRunning)
-            xVal *= runSpdModifier;
-
-        //Create Vec2 for the Velocity
-        Vector2 targetVelocity = new Vector2(xVal, rb2d.velocity.y);
-
-        //Set the players Velocity
-        rb2d.velocity = targetVelocity;
-
-        //If looking right and press left, turn left
-        if (facingRight && dir < 0)
+        //Code for Move
+        void Move(float dir, bool jumpFlag)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
-            facingRight = false;
+            //Jump
+
+            if (isGrounded && jumpFlag)
+            {
+                isGrounded = false;
+                jumpFlag = false;
+                rb2d.AddForce(new Vector2(0f, jumpPower));
+            }
+
+            //Move and Run
+            //Set value of x using dir and speed
+            float xVal = dir * spd * 100 * Time.fixedDeltaTime;
+
+            if (isRunning)
+                xVal *= runSpdModifier;
+
+            //Create Vec2 for the Velocity
+            Vector2 targetVelocity = new Vector2(xVal, rb2d.velocity.y);
+
+            //Set the players Velocity
+            rb2d.velocity = targetVelocity;
+
+            //If looking right and press left, turn left
+            if (facingRight && dir < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                facingRight = false;
+            }
+            //If looking left and press right, turn to the right
+            else if (!facingRight && dir > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                facingRight = true;
+            }
+
+            animator.SetFloat("xVelocity", Mathf.Abs(rb2d.velocity.x));
         }
-        //If looking left and press right, turn to the right
-        else if (!facingRight && dir > 0)
+
+        public void Die()
         {
-            transform.localScale = new Vector3(1, 1, 1);
-            facingRight = true;
+            isDead = true;
+            FindObjectOfType<LevelManager>().Restart();
         }
 
-        animator.SetFloat("xVelocity", Mathf.Abs(rb2d.velocity.x));
-    }
-    public void Die()
+        public void ResetPlayer()
+        {
+            isDead = false;
+        }
+
+    Vector2 CalculateProjectileVelocity(Vector2 startPoint, Vector2 targetPoint, float timeToTarget)
     {
-        isDead = true;
-        FindObjectOfType<LevelManager>().Restart();
+        float gravity = Physics2D.gravity.magnitude;
+        Vector2 displacementY = targetPoint - startPoint;
+        displacementY.y = 0;
+        float y = displacementY.magnitude;
+        float Vyi = (y - 0.5f * gravity * timeToTarget * timeToTarget) / timeToTarget;
+        Vector2 velocityY = new Vector2(0, Vyi);
+        Vector2 displacementXZ = targetPoint - startPoint;
+        displacementXZ.y = 0;
+        float xz = displacementXZ.magnitude;
+        float Vxi = xz / timeToTarget;
+        Vector2 velocityXZ = displacementXZ.normalized * Vxi;
+        return velocityXZ + velocityY.normalized;
     }
 
-    public void ResetPlayer()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        isDead = false;
-    }
-
-    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float t)
-    {
-        Vector2 distance = target - origin;
-
-        float velocityX = distance.x / t;
-        float velocityY = distance.y / t + 0.5f * Mathf.Abs(Physics2D.gravity.y);
-        Vector2 XnY = new Vector2(velocityX, velocityY);
-        return XnY;
+        if (collision.gameObject.CompareTag("Enemy")) 
+        {
+            Destroy(gameObject);
+        }
     }
 
 }

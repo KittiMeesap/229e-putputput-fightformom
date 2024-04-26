bc1c8faf3a63171d9456480d380bd847339f862a
kitti <tkt.ms@hotmail.com>
2024-04-26 13:43:12 +0700

    Update Button 0.1

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void HowToPlay()
+    { 
+        SceneManager.LoadSceneAsync(2);
+    }
+}
